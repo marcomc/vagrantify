60485337290e4bd9af27a71b3ac641c12d438520
Marco Massari Calderone <marco@marcomc.com>
2014-01-21 22:56:10 +0000

    add the possibility to personalise the OS name

--- common_vm_settings.sh
+++ common_vm_settings.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+#
 # Setup vm
+#
 # http://docs.vagrantup.com/v2/boxes/base.html
 # https://github.com/fespinoza/checklist_and_guides/wiki/Creating-a-vagrant-base-box-for-ubuntu-12.04-32bit-server
+# http://blog.vandenbrand.org/2012/02/21/creating-a-centos-6-2-base-box-for-vagrant/
 
 # to modify so that the os is autodetectd via /etc/os-release or /etc/redhat-release files
-OS="$1" # 'ubuntu' or 'centos'
+OS="$1" # 'ubuntu64-server-xxx' or 'centos65xx'
 #VARIANT="$2"
 VAGRANT_HOSTNAME="vagrant-$OS" #-$VARIANT"
 VAGRANT_DOMAIN="vagrantup.com"
@@ -13,7 +16,12 @@ MAIN_ACCOUNT="vagrant"
 MAIN_ACCOUNT_PWD="vagrant"
 ADMIN_GROUP="admin"
 
-source "./setup_"$OS".sh"
+# this checks what kind of OS we want to vagrantify, allowing the definition of personalised names for the os 
+if echo $OS | grep centos - -q ;then
+    source "./setup_cenotos.sh"
+elif echo $OS | grep ubuntu - -q ; then
+    source "./setup_ubuntu.sh"
+fi
 
 # set the domain name
 sed -i "/127.0.1.1/d" /etc/hosts #removes existing definitions
@@ -34,6 +42,10 @@ done
 sed -i "/UseDNS/d" /etc/ssh/sshd_config
 # add the UseDNS=no setting to the sshd_config file
 echo 'UseDNS no' >> /etc/ssh/sshd_config
+
+# set the ssh server to listen on port 2222
+sed -i '/Port /d' /etc/ssh/sshd_config
+echo 'Port 2222' >> /etc/ssh/sshd_config
 
 # set the root password
 echo root:$ROOT_PWD | chpasswd
@@ -60,4 +72,8 @@ chown $MAIN_ACCOUNT:$MAIN_ACCOUNT /home/$MAIN_ACCOUNT/.ssh/vagrant.pub
 cat /home/$MAIN_ACCOUNT/.ssh/vagrant.pub >> /home/$MAIN_ACCOUNT/.ssh/authorized_keys
 # install the insecure vagrant private key pair
 
-
+# delete the user 'user' wich we probably did set up when we created the vm
+# on ubunto this command will fail if we are running the script as sudo from the user 'user' itself
+if [ -d "/home/user" ]; then
+    userdel -f -r user
+fi

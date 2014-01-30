78833d6083e9c3e83d6a777b7a28adffeb867997
Marco Massari Calderone <marco@marcomc.com>
2014-01-30 23:11:21 +0000

    Setup of IPTABLES

--- setup_centos_webserver.sh
+++ setup_centos_webserver.sh
@@ -4,6 +4,7 @@
 DOMAIN_NAME=localhost
 CA_ROOT="/etc/pki/CA"
 TLS_ROOT="/etc/pki/tls"
+IPTABLE_SAVE_FILE="/etc/sysconfig/iptables"
 /vagrant/vagrantme/setup_ca_and_certificate.sh $CA_ROOT $DOMAIN_NAME
 echo "Copying Certificate and Private Key to the TLS folder for later use by mod_ssl"
 cp $CA_ROOT/private/$DOMAIN_NAME.key  $TLS_ROOT/private/$DOMAIN_NAME.key
@@ -85,4 +86,25 @@ then
 else
     curl -# http://localhost/multispace_test.html 2> /dev/null
     echo "X-Mod-Pagespeed is removing space properly"
-fi
+
+# IPTABLES Firewall setupt
+echo "Enabling IPTABLES to accept connection exclusively for SSH, HTTP, HTTPS and CacheProxy"
+cat <<'EOF' > $IPTABLE_SAVE_FILE
+*filter
+:INPUT ACCEPT [0:0]
+:FORWARD ACCEPT [0:0]
+:OUTPUT ACCEPT [0:0]
+-A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+-A INPUT -p icmp -j ACCEPT
+-A INPUT -i lo -j ACCEPT
+-A INPUT -m state --state NEW -m tcp -p tcp --dport 22 -j ACCEPT
+-A INPUT -m state --state NEW -m tcp -p tcp --dport 80 -j ACCEPT
+-A INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
+-A INPUT -m state --state NEW -m tcp -p tcp --dport 6081 -j ACCEPT
+-A INPUT -j REJECT --reject-with icmp-host-prohibited
+-A FORWARD -j REJECT --reject-with icmp-host-prohibited
+COMMIT
+EOF
+/etc/init.d/iptables restart
+
+echo "Webserver Configuration completed!"
